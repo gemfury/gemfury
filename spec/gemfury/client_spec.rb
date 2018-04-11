b5e2cce6a3cb568d90a7199942eb4fcf8b2538bd
Mark John Buenconsejo <hi@markjeee.com>
2018-04-11 14:00:52 +0800

    Refactored app_spec and auth_spec to use `expect` method.

--- spec/gemfury/client_spec.rb
+++ spec/gemfury/client_spec.rb
@@ -10,37 +10,37 @@ describe Gemfury::Client do
       stub_post("access_token").to_return(:body => access_token_fixture)
 
       token = @client.login('test@test.com', '123')
-      token.should eq('access_token' => 'TestToken')
+      expect(token).to eq('access_token' => 'TestToken')
 
-      a_post("access_token").with(
-        :body => { :email => 'test@test.com', :password => '123' }
-      ).should have_been_made.once
+      expect(a_post("access_token").with(
+               :body => { :email => 'test@test.com', :password => '123' }
+             )).to have_been_made.once
     end
 
     it 'should raise an unauthorized error for bad credentials' do
       stub_post("access_token").to_return(:status => 401)
 
-      lambda do
-        @client.login('test@test.com', '123')
-      end.should raise_error(Gemfury::Unauthorized)
+      expect(lambda {
+               @client.login('test@test.com', '123')
+             }).to raise_error(Gemfury::Unauthorized)
     end
   end
 
   describe '#get_access_token' do
     it 'should wrap #login with same args' do
-      @client.should_receive(:login).
-              with('test@test.com', '123', :as => 't123').
-              and_return('access_token' => 'TestToken')
+      expect(@client).to receive(:login).
+                           with('test@test.com', '123', :as => 't123').
+                           and_return('access_token' => 'TestToken')
 
       token = @client.get_access_token('test@test.com', '123', :as => 't123')
-      token.should eq('TestToken')
+      expect(token).to eq('TestToken')
     end
 
     it 'should proxy #login exceptions' do
-      lambda do
-        stub_post("access_token").to_return(:status => 401)
-        @client.get_access_token('test@test.com', '123')
-      end.should raise_error(Gemfury::Unauthorized)
+      expect(lambda {
+               stub_post("access_token").to_return(:status => 401)
+               @client.get_access_token('test@test.com', '123')
+             }).to raise_error(Gemfury::Unauthorized)
     end
   end
 
@@ -48,38 +48,40 @@ describe Gemfury::Client do
     before { stub_api_method.to_return(:status => 401) }
 
     it 'should throw an authentication error without an api key' do
-      lambda do
-        @client.user_api_key = nil
-        send_api_request
-      end.should raise_error(Gemfury::Unauthorized)
+      expect(lambda {
+               @client.user_api_key = nil
+               send_api_request
+             }).to raise_error(Gemfury::Unauthorized)
     end
 
     it 'should throw an authentication error on a bad key' do
-      lambda do
-        @client.user_api_key = 'MyWrongApiKey'
-        send_api_request
-      end.should raise_error(Gemfury::Unauthorized)
+      expect(lambda {
+               @client.user_api_key = 'MyWrongApiKey'
+               send_api_request
+             }).to raise_error(Gemfury::Unauthorized)
     end
   end
 
   shared_examples 'graceful handler of errors' do
     it 'should raise NotFound error for a non-existent user' do
       stub_api_method.to_return(:status => 404)
-      lambda {
-        send_api_request
-      }.should raise_exception(Gemfury::NotFound)
+
+      expect(lambda {
+               send_api_request
+             }).to raise_exception(Gemfury::NotFound)
     end
 
     it 'should throw a conflict error when resource is locked' do
       stub_api_method.to_return(:status => 409)
-      lambda {
-        send_api_request
-      }.should raise_exception(Gemfury::Conflict)
+
+      expect(lambda {
+               send_api_request
+             }).to raise_exception(Gemfury::Conflict)
     end
   end
 
   describe '#account_info' do
-    let(:stub_api_method)  { stub_get("users/me") }
+    let(:stub_api_method)  { stub_get('users/me') }
     let(:send_api_request) { @client.account_info }
 
     it_should_behave_like 'API without authentication'
@@ -92,8 +94,8 @@ describe Gemfury::Client do
 
       it 'should return valid account info' do
         out = @client.account_info
-        out['username'].should eq('user1')
-        a_get("users/me").should have_been_made
+        expect(out['username']).to eq('user1')
+        expect(a_get("users/me")).to have_been_made
       end
     end
   end
@@ -116,13 +118,13 @@ describe Gemfury::Client do
 
       it 'should upload valid gems' do
         @client.push_gem(fixture('fury-0.0.2.gem'))
-        a_post("uploads", 2).should have_been_made
+        expect(a_post("uploads", 2)).to have_been_made
       end
     end
   end
 
   describe '#list' do
-    let(:stub_api_method)  { stub_get("gems") }
+    let(:stub_api_method)  { stub_get('gems') }
     let(:send_api_request) { @client.list }
 
     it_should_behave_like 'API without authentication'
@@ -135,15 +137,16 @@ describe Gemfury::Client do
 
       it 'should list uploaded gems' do
         gems_list = send_api_request
-        a_get("gems").should have_been_made
-        gems_list.size.should eq(1)
-        gems_list.first['name'].should eq('example')
+        expect(a_get("gems")).to have_been_made
+
+        expect(gems_list.size).to eq(1)
+        expect(gems_list.first['name']).to eq('example')
       end
     end
   end
 
   describe '#versions' do
-    let(:stub_api_method)  { stub_get("gems/example/versions") }
+    let(:stub_api_method)  { stub_get('gems/example/versions') }
     let(:send_api_request) { @client.versions('example') }
 
     it_should_behave_like 'API without authentication'
@@ -156,15 +159,16 @@ describe Gemfury::Client do
 
       it 'should list gem versions' do
         versions = send_api_request
-        a_get("gems/example/versions").should have_been_made
-        versions.size.should eq(2)
-        versions.first['slug'].should eq('example-0.0.1')
+        expect(a_get("gems/example/versions")).to have_been_made
+
+        expect(versions.size).to eq(2)
+        expect(versions.first['slug']).to eq('example-0.0.1')
       end
     end
   end
 
   describe '#yank_version' do
-    let(:stub_api_method)  { stub_delete("gems/example/versions/0.0.1") }
+    let(:stub_api_method)  { stub_delete('gems/example/versions/0.0.1') }
     let(:send_api_request) { @client.yank_version('example', '0.0.1') }
 
     it_should_behave_like 'API without authentication'
@@ -177,13 +181,13 @@ describe Gemfury::Client do
 
       it 'should delete selected package version' do
         send_api_request
-        a_delete("gems/example/versions/0.0.1").should have_been_made
+        expect(a_delete("gems/example/versions/0.0.1")).to have_been_made
       end
     end
   end
 
   describe '#list_collaborators' do
-    let(:stub_api_method)  { stub_get("collaborators") }
+    let(:stub_api_method)  { stub_get('collaborators') }
     let(:send_api_request) { @client.list_collaborators }
 
     it_should_behave_like 'API without authentication'
@@ -196,15 +200,16 @@ describe Gemfury::Client do
 
       it 'should list account collaborators' do
         gems_list = send_api_request
-        a_get("collaborators").should have_been_made
-        gems_list.size.should eq(2)
-        gems_list.first['username'].should eq('user1')
+        expect(a_get("collaborators")).to have_been_made
+
+        expect(gems_list.size).to eq(2)
+        expect(gems_list.first['username']).to eq('user1')
       end
     end
   end
 
   describe '#add_collaborators' do
-    let(:stub_api_method)  { stub_put("collaborators/user1") }
+    let(:stub_api_method)  { stub_put('collaborators/user1') }
     let(:send_api_request) { @client.add_collaborator('user1') }
 
     it_should_behave_like 'API without authentication'
@@ -215,7 +220,8 @@ describe Gemfury::Client do
       it 'should add a collaborator' do
         stub_api_method
         send_api_request
-        a_put("collaborators/user1").should have_been_made
+
+        expect(a_put("collaborators/user1")).to have_been_made
       end
 
       it_should_behave_like 'graceful handler of errors'
@@ -223,7 +229,7 @@ describe Gemfury::Client do
   end
 
   describe '#remove_collaborators' do
-    let(:stub_api_method)  { stub_delete("collaborators/user1") }
+    let(:stub_api_method)  { stub_delete('collaborators/user1') }
     let(:send_api_request) { @client.remove_collaborator('user1') }
 
     it_should_behave_like 'API without authentication'
@@ -234,7 +240,8 @@ describe Gemfury::Client do
       it 'should remove an existing collaborator' do
         stub_api_method
         send_api_request
-        a_delete("collaborators/user1").should have_been_made
+
+        expect(a_delete("collaborators/user1")).to have_been_made
       end
 
       it_should_behave_like 'graceful handler of errors'
@@ -242,7 +249,7 @@ describe Gemfury::Client do
   end
 
   describe '#git_repos' do
-    let(:stub_api_method)  { stub_get("git/repos/me") }
+    let(:stub_api_method)  { stub_get('git/repos/me') }
     let(:send_api_request) { @client.git_repos }
 
     it_should_behave_like 'API without authentication'
@@ -255,15 +262,16 @@ describe Gemfury::Client do
 
       it 'should list git repos' do
         repo_list = send_api_request['repos']
-        a_get("git/repos/me").should have_been_made
-        repo_list.size.should eq(1)
-        repo_list.first['name'].should eq('example')
+        expect(a_get("git/repos/me")).to have_been_made
+
+        expect(repo_list.size).to eq(1)
+        expect(repo_list.first['name']).to eq('example')
       end
     end
   end
 
   describe '#git_reset' do
-    let(:stub_api_method)  { stub_delete("git/repos/me/example") }
+    let(:stub_api_method)  { stub_delete('git/repos/me/example') }
     let(:send_api_request) { @client.git_reset('example') }
 
     it_should_behave_like 'API without authentication'
@@ -278,13 +286,13 @@ describe Gemfury::Client do
 
       it 'should yank specified git repo' do
         send_api_request
-        a_delete("git/repos/me/example").should have_been_made
+        expect(a_delete('git/repos/me/example')).to have_been_made
       end
     end
   end
 
   describe '#git_rename' do
-    let(:stub_api_method)  { stub_patch("git/repos/me/example") }
+    let(:stub_api_method)  { stub_patch('git/repos/me/example') }
     let(:send_api_request) { @client.git_update('example', {
       :repo => { :name => 'new_example' }
     }) }
@@ -301,13 +309,13 @@ describe Gemfury::Client do
 
       it 'should update specified git repo' do
         send_api_request
-        a_patch("git/repos/me/example").should have_been_made
+        expect(a_patch('git/repos/me/example')).to have_been_made
       end
     end
   end
 
   describe '#git_rebuild' do
-    let(:stub_api_method)  { stub_post("git/repos/me/example/builds") }
+    let(:stub_api_method)  { stub_post('git/repos/me/example/builds') }
     let(:send_api_request) { @client.git_rebuild('example', @params) }
 
     it_should_behave_like 'API without authentication'
@@ -322,18 +330,18 @@ describe Gemfury::Client do
 
       it 'should rebuild specified git repo' do
         send_api_request
-        a_post("git/repos/me/example/builds", {
-          :api_format => :text
-        }).should have_been_made
+        expect(a_post('git/repos/me/example/builds', {
+                        :api_format => :text
+        })).to have_been_made
       end
 
       context 'with specified revision' do
         before { @params = { :build => { :revision => 'tag-name' }} }
         it 'should rebuild git repo at specified revision' do
           send_api_request
-          a_post("git/repos/me/example/builds", {
-            :api_format => :text
-          }).with(:body => @params).should have_been_made
+          expect(a_post("git/repos/me/example/builds", {
+                          :api_format => :text
+                        }).with(:body => @params)).to have_been_made
         end
       end
     end
