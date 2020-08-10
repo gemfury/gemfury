085568bd316eeb161a476fc8fe80932fba814f69
Ian <ikatz@drizly.com>
2020-08-10 08:29:17 -0400

    Fully switch to duck typing
    Explicitly check for IO methods that are required by UploadIO and Faraday. 
    
    Explicitly provide a value for the IO method "path", which might be missing from classes like StringIO

--- lib/faraday/request/multipart_with_file.rb
+++ lib/faraday/request/multipart_with_file.rb
@@ -6,20 +6,26 @@ module Faraday
   class Request::MultipartWithFile < Faraday::Middleware
     def call(env)
 
-      # Faraday seems to expect a few IO methods to be available, but that's all:
-      # https://github.com/lostisland/faraday/blob/master/lib/faraday/file_part.rb
-      # :length seems to be an optional one
-      #
-      # UploadIO also seems to do a duck typing check for :read, with :path optional
-      # https://www.rubydoc.info/gems/multipart-post/2.0.0/UploadIO:initialize
-      required_io_methods = ["rewind", "read", "close"].map(&:to_sym)
-
       if env[:body].is_a?(Hash)
+        
+        # Check for IO (and IO-like objects, like Zip::InputStream) in the request, 
+        # which represent data to be uploaded.  Replace these with Faraday 
         env[:body].each do |key, value|
-          # Allow both IO derivates, and IO-like objects via duck typing
-          # (e.g. Zip::InputStream) 
-          if value.is_a?(IO) || required_io_methods.all? { |m| value.respond_to? (m) }
-            env[:body][key] = Faraday::UploadIO.new(value, mime_type(value), value.path)
+          
+          # Faraday seems to expect a few IO methods to be available, but that's all:
+          # https://github.com/lostisland/faraday/blob/master/lib/faraday/file_part.rb
+          # :length seems to be an optional one
+          #
+          # UploadIO also seems to do a duck typing check for :read, with :path optional
+          # https://www.rubydoc.info/gems/multipart-post/2.0.0/UploadIO:initialize
+          # 
+          # We attempt to make our duck typing compatible with their duck typing
+          if value.respond_to?(:read) && value.respond_to?(:rewind) && value.respond_to?(:close)
+            # Mimic UploadIO's handling of inputs like StringIO, which don't have a :path method
+            local_path = value.respond_to?(:path) ? value.path : 'local.path'
+            local_mime = value.respond_to?(:path) ? mime_type(value) : 'application/octet-stream'
+            
+            env[:body][key] = Faraday::UploadIO.new(value, local_mime, local_path)
           end
         end
       end
