2304bc5f9ba6998f7e5fe4839837198d3bb19089
Ian <ikatz@drizly.com>
2020-08-10 08:32:12 -0400

    Explicitly handle existence of path method in mime_type
    Since mime_type expects to receive an object that responds to :path, explicitly check for that method within the function itself

--- lib/faraday/request/multipart_with_file.rb
+++ lib/faraday/request/multipart_with_file.rb
@@ -7,25 +7,24 @@ module Faraday
     def call(env)
 
       if env[:body].is_a?(Hash)
-        
-        # Check for IO (and IO-like objects, like Zip::InputStream) in the request, 
-        # which represent data to be uploaded.  Replace these with Faraday 
+
+        # Check for IO (and IO-like objects, like Zip::InputStream) in the request,
+        # which represent data to be uploaded.  Replace these with Faraday
         env[:body].each do |key, value|
-          
+
           # Faraday seems to expect a few IO methods to be available, but that's all:
           # https://github.com/lostisland/faraday/blob/master/lib/faraday/file_part.rb
           # :length seems to be an optional one
           #
           # UploadIO also seems to do a duck typing check for :read, with :path optional
           # https://www.rubydoc.info/gems/multipart-post/2.0.0/UploadIO:initialize
-          # 
+          #
           # We attempt to make our duck typing compatible with their duck typing
           if value.respond_to?(:read) && value.respond_to?(:rewind) && value.respond_to?(:close)
             # Mimic UploadIO's handling of inputs like StringIO, which don't have a :path method
             local_path = value.respond_to?(:path) ? value.path : 'local.path'
-            local_mime = value.respond_to?(:path) ? mime_type(value) : 'application/octet-stream'
-            
-            env[:body][key] = Faraday::UploadIO.new(value, local_mime, local_path)
+
+            env[:body][key] = Faraday::UploadIO.new(value, mime_type(value), local_path)
           end
         end
       end
@@ -36,6 +35,9 @@ module Faraday
     private
 
     def mime_type(file)
+      default = 'application/octet-stream'
+      return default unless file.respond_to?(:path)
+
       case file.path
       when /\.jpe?g/i
         'image/jpeg'
@@ -44,7 +46,7 @@ module Faraday
       when /\.png$/i
         'image/png'
       else
-        'application/octet-stream'
+        default
       end
     end
   end
